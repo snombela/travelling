5c757a5a3033b80168965f632af94d17faa3741b
Sofía <sofianombela@gmail.com>
2019-03-19 21:01:22 +0100

    delete comment

--- back-server/routes/comment.js
+++ back-server/routes/comment.js
@@ -7,19 +7,47 @@ router.post("/comment", (req, res, next) => {
   if (req.isAuthenticated()) {
     Comment.create({ content: req.body.content, title: req.body.title, userId: req.user._id })
       .then(comment => {
-          Location.findByIdAndUpdate(req.body.locationId,
-            {$push: {comments: comment._id}}
-            ).then(() => {
-                res.status(200).json(comment);
-            }).catch(err => {
-                console.log(err)
-                res.status(404).json({ message: "Location no update" });
-              });
+        Location.findByIdAndUpdate(req.body.locationId,
+          { $push: { comments: comment._id } }
+        ).then(() => {
+          res.status(200).json(comment);
+        }).catch(err => {
+          console.log(err)
+          res.status(404).json({ message: "Location no update" });
+        });
       })
       .catch(err => {
-          console.log(err)
+        console.log(err)
         res.status(404).json({ message: "Comment doesn't create" });
       });
+  } else {
+    res.status(403).json({ message: "Unauthorized" });
+  }
+});
+
+router.delete("/comment/:commentId", (req, res, next) => {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.commentId)
+      .then(comment => {
+        console.log(comment.userId)
+        console.log(req.user._id)
+        console.log(comment.userId.equals(req.user._id))
+        if (comment.userId.equals(req.user._id)) {
+          Comment.findByIdAndDelete(req.params.commentId)
+            .then(() => {
+              res.status(200).json({ message: "The comment has deleted successfully!" });
+            }).catch(err => {
+              console.log(err)
+              res.status(404).json({ message: "An error has occurred!" });
+            });
+        } else {
+          res.status(403).json({ message: "This user isn't authorized" });
+        }
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(404).json({ message: "Comment doesn't exists" });
+      })
   } else {
     res.status(403).json({ message: "Unauthorized" });
   }
@@ -29,14 +57,14 @@ router.post("/comment", (req, res, next) => {
 
 router.get("/location/:locationId/comments", (req, res, next) => {
   Location.findById(req.params.locationId)
-  .populate({path: "comments", populate: {path: "userId"}})
-  .then(response => {
-    res.status(200).json(response.comments);
-  })
-  .catch(err => {
-    console.log(err)
-    res.status(404).json({ message: "Location doesn't found" });
-  });
+    .populate({ path: "comments", populate: { path: "userId" } })
+    .then(response => {
+      res.status(200).json(response.comments);
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(404).json({ message: "Location doesn't found" });
+    });
 })
 
 module.exports = router;
